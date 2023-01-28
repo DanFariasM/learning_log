559eb6cd5ccbcf436e7ba9a01791b20fc4222e90
DanFariasM <danfariasm@gmail.com>
2023-01-27 23:26:25 -0500

    Creating the topics page.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,6 @@ app_name = "learning_logs"
 urlpatterns = [
     # Home page
     path("", views.index, name="index"),
+    # Page that shows all the topics.
+    path("topics/", views.topics, name="topics"),
 ]
